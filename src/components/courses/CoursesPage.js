65a4cfc163c259c44a7f31a843986e400aaa3999
Sean Campbell <scampbell@mutley.net.au>
2020-02-17 21:37:31 +1100

    Added full redux flow

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import * as courseActions from '../../redux/actions/courseActions'
+import PropTypes from 'prop-types'
+import {bindActionCreators} from 'redux'
 
 class CoursesPage extends React.Component {
     state = {
@@ -14,6 +18,9 @@ class CoursesPage extends React.Component {
 
     handleSubmit = event => {
         event.preventDefault();
+        console.log(this.state.course);
+        console.log(this.props.actions);
+        this.props.actions.createCourse(this.state.course);
     };
 
     render() {
@@ -23,9 +30,30 @@ class CoursesPage extends React.Component {
                     <h3>Add Course</h3>
                     <input type="text" onChange={this.handleChange} value={this.state.course.title} />
                     <input type="submit" value="Save"/>
+
+                    {this.props.courses.map(course => (
+                        <div key={course.title}>{course.title}</div>
+                    ))}    
                 </form>
             )
     }
 }
 
-export default CoursesPage
+CoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state){
+    return {
+         courses: state.courses
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,7 +1,9 @@
+import * as actionTypes from '../actions/actionTypes'
+
 export default function courseReducer(state = [], action) {
-    switch(action){
-        case "CREATE_COURSE": 
-            return [...state, {...action.course }];
+    switch(action.type){
+        case actionTypes.CREATE_COURSE: 
+            return [...state, {...action.course}];
         default:
             return state;
     }
