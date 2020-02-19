859a99495cae8a8ddc769040276d4f334c0b02b0
Sean Campbell <scampbell@mutley.net.au>
2020-02-19 21:37:26 +1100

    Added first thunk for course retrieval

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,6 +1,21 @@
 import * as actionTypes from './actionTypes'
+import * as courseApi from "../../api/courseApi"
 
 export function createCourse(course) {
-    return {type: actionTypes.CREATE_COURSE, course};
+    return {type: actionTypes.CREATE_COURSE, course };
+}
+
+function loadCoursesSuccess(courses) {
+    return {type: actionTypes.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+    return function (dispatch) {
+        return courseApi.getCourses().then(courses => {
+            dispatch(loadCoursesSuccess(courses));
+        }).catch( error => {
+            throw error;
+        })
+    }
 }
 
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -4,6 +4,8 @@ export default function courseReducer(state = [], action) {
     switch(action.type){
         case actionTypes.CREATE_COURSE: 
             return [...state, {...action.course}];
+        case actionTypes.LOAD_COURSES_SUCCESS:
+            return action.courses;
         default:
             return state;
     }
